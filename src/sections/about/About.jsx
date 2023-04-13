b98463a9f054b37533f66b3a572149b3aecce73f
btarnow <beckytarnowski@gmail.com>
2023-04-13 13:03:57 -0400

    header section frame complete and about section started

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,8 +1,45 @@
 import './about.css'
+import RowinPic from '../../assets/Rowin-and-I.png'
+import Resume from '../../assets/resume.pdf'
+import {HiDownload} from 'react-icons/hi'
+import Card from '../../components/Card'
+import data from './data'
+
 
 const About = () => {
   return (
-    <section id="about"><h2>About</h2></section>
+    <section id="about">
+      <div className="container about__container">
+        <div className="about__left">
+          <div className="about__portrait">
+            <img src={RowinPic} alt="Becky with her dog Rowin" />
+          </div>
+        </div>
+        <div className="about__right">
+          <h2>About Me</h2>
+          <div className='about__cards'>
+            {
+              data.map(item => (
+                <Card key={item.id} className="about__card" >
+                  <span className='about__card-icon'>{item.icon}</span>
+                  <h5>{item.title}</h5>
+                  <small>{item.desc}</small>
+                </Card>
+              ))
+            }
+          </div>
+          <p>
+              Text about myself. Text about myself. Text about myself. 
+              Text about myself. Text about myself. Text about myself.
+            </p>
+            <p>
+              Text about myself. Text about myself. Text about myself. 
+              Text about myself. Text about myself. Text about myself.  
+            </p>
+            <a href={Resume} download className='btn primary'>Download Resume<HiDownload /></a>
+        </div>
+      </div>
+    </section>
   )
 } 
 
--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -6,22 +6,22 @@ const Header = () => {
   return (
     <header id="header">
       <div className="container header__container">
-
-        <div className="header_profile">
-          <img src={ProfilePic} alt="Profile" />
+        <div className="header__profile">
+          <img src={ProfilePic} alt="Header Portrait" />
         </div>
 
         <h3>Becky Tarnowski</h3>
         <p>
           Text here about myself.
         </p>
-
-        <div classNate="header__cta">
+        
+        {/* Header "Call To Action area" */}
+        <div className="header__cta">
           <a href="#contact" className="btn primary">Let's talk</a>
           <a href="#projects" className="btn light">My Work</a>
         </div>
 
-        <div className="header_socials">
+        <div className="header__socials">
           {
             data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
           }
