b7d7b4df7578f729088feed18b84b3be4add5234
btarnow <beckytarnowski@gmail.com>
2023-04-25 11:18:24 -0400

    updates to tech stack logos

--- src/sections/skills/data.js
+++ src/sections/skills/data.js
@@ -5,7 +5,7 @@ import postgresLogo from '../../assets/postgresql.webp'
 import cssLogo from '../../assets/css.png'
 import htmlLogo from '../../assets/html.png'
 import sqlLogo from '../../assets/sql.png'
-import sqlalchemyLogo from '../../assets/SQLAlchemy.jpeg'
+import sqlalchemyLogo from '../../assets/sqlalch.png'
 import jinjaLogo from '../../assets/jinja.png'
 // import reactLogo from '../../assets/react.png'
 
@@ -15,9 +15,9 @@ const logos = [
     {id: 2, logo: flaskLogo, name: 'Flask'},
     {id: 3, logo: javascriptLogo, name: 'JavaScript'},
     {id: 4, logo: postgresLogo, name: 'PostgreSQL'},
-    {id: 5, logo: cssLogo, name: 'CSS'},
-    {id: 6, logo: htmlLogo, name: 'HTML'},
-    {id: 7, logo: sqlLogo, name: 'SQL'},
+    {id: 5, logo: sqlLogo, name: 'SQL'},
+    {id: 6, logo: cssLogo, name: 'CSS'},
+    {id: 7, logo: htmlLogo, name: 'HTML'},
     {id: 8, logo: sqlalchemyLogo, name: 'SQLAlchemy'},
     {id: 9, logo: jinjaLogo, name: 'Jinja'},
     // {id: 10, logo: reactLogo, name: 'React'},
