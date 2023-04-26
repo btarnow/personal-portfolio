90b220d6dfc1aee64f52df96dbb56b6a23c4f95d
btarnow <beckytarnowski@gmail.com>
2023-04-25 21:14:25 -0400

    basic footer complete

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,8 +1,25 @@
 import './footer.css'
+import {links, socials} from './data'
+
 
 const Footer = () => {
   return (
-    <footer><h2>Footer</h2></footer>
+    <footer>
+      <div className='container footer__container'>
+        <ul className='nav__menu'>
+          {
+            links.map(footerLink => <li key={footerLink.id}>
+              <a href={footerLink.link}>{footerLink.title}</a></li>)
+          }
+        </ul>
+        <div className='footer__socials'>
+          {
+            socials.map(social => <a key={social.id} href={social.link} 
+            target='_blank' rel='noopener noreferrer'>{social.icon}</a>)
+          }
+        </div>
+      </div>
+    </footer>
   )
 }
 
